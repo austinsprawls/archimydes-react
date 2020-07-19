3604aa7600d19736bc0b76eaa738d6d6525f38fc
Austin Sprawls <austinsprawls@gmail.com>
2020-07-19 06:25:21 -0700

    removed extra starter code to clean up

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
 import loginReducer from '../scenes/login/loginSlice';
 import createStoryReducer from '../scenes/createStory/createStorySlice';
 import listStoryReducer from '../scenes/listStories/listStoriesSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     login: loginReducer,
     createStory: createStoryReducer,
     listStories: listStoryReducer
